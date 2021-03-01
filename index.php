11764711e2767d3f233dc073dfcd5df1ca9ee48d
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-03-01 14:05:12 +0100

    Modif

--- index.php
+++ index.php
@@ -13,3 +13,64 @@
  * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
  * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
  */
+require "./Classes/DB.php";
+$db = DB::getInstance();
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $search = $db->prepare("SELECT * FROM user");
+
+    $state = $search->execute();
+
+    echo "<div class='container'>";
+    if($state) {
+        foreach ($search->fetchAll() as $user) {
+            echo "<div class='user'>Utilisateur $user[id]: $user[prenom] $user[nom], habbitant $user[rue] numéro $user[numero] à $user[ville] 
+                  en $user[pays] avec cette adresse mail: $user[mail]. </div>";
+        }
+    }
+    echo "</div>";
+
+    echo "<br>";
+
+    $search = $db->prepare("SELECT * FROM user ORDER BY id DESC");
+
+    $state = $search->execute();
+
+    echo "<div class='container'>";
+    if($state) {
+        foreach ($search->fetchAll() as $user) {
+            echo "<div class='user'>Utilisateur $user[id]: $user[prenom] $user[nom], habbitant $user[rue] numéro $user[numero] à $user[ville] 
+                  en $user[pays] avec cette adresse mail: $user[mail]. </div>";
+        }
+    }
+    echo "</div>";
+
+    echo "<br>";
+
+    $search = $db->prepare("SELECT * FROM user");
+
+    $state = $search->execute();
+
+    echo "<div class='container'>";
+    if($state) {
+        foreach ($search->fetchAll() as $user) {
+            echo "<div class='user'>Utilisateur $user[id]: $user[prenom] $user[nom]</div>";
+        }
+    }
+    echo "</div>";
+?>
+</body>
+</html>
